8a82a4f5b3c361087f6c9706e14c015a5bb56648
Alexandr Chistjakov <alchist@mera.ru>
2012-08-13 15:47:28 +0400

    Some changes

--- Test.py
+++ Test.py
@@ -2,7 +2,4 @@ import subprocess
 x=subprocess.Popen("ls -1", shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).stdout.read().split('\n')
 x.pop()
 for y in x:
-	#s="mv"+' '+y+' '+ y[-10:]
-	#print (y)
 	subprocess.call(['mv',y,y[-10:]] )
-#subprocess.call(["ls","-l"])
